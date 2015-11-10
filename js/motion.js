edfca31f3b1a9f4209809ecfda6a2e62acf722bb
Chris Frank <chris@thenepotist.com>
2015-11-09 19:49:22 -0500

    let there be motion

--- js/motion.js
+++ js/motion.js
@@ -4,4 +4,14 @@ $(function(){
       $(this).append('<video autoplay loop><source src="/img/homepage-video-loop-web.mp4" type="video/mp4"></video>')
     })
   }
+  $('a[rel=player]').on('click', function(event) {
+    $post = $(this).closest('.post')
+    if ($post.length > 0) {
+      event.preventDefault()
+      frame = document.createElement('iframe')
+      frame.src = this.href
+      frame.className = "inline-video-player"
+      $post.addClass('video-added').append(frame)
+    }
+  })
 })
