a73aea57f37b40e06e9f90e3c4dd764c99adbb2b
Chris Frank <chris@thenepotist.com>
2015-11-11 13:44:15 -0500

    removed console.log on homepage script

--- js/homepage.js
+++ js/homepage.js
@@ -2,7 +2,6 @@ jQuery.fn.random = function(lowerLimit, upperLimit) {
   var lowerLimit = lowerLimit || 0
   var upperLimit = upperLimit || this.length
   var randomIndex = Math.floor(lowerLimit + Math.random() * (upperLimit-lowerLimit));
-  console.log(randomIndex)
   return jQuery(this[randomIndex]);
 }
 
@@ -44,7 +43,6 @@ $(function(){
       var $images = $partnerImages.find('li')
       var $bullet = $images.random(10, $images.length)
       var $target = $images.random(0, 10)
-      //console.log($target.prevAll().length)
       swap($bullet, $target)
     }, 1000)
   }
