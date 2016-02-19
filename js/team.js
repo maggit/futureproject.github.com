3ecafcedaa86033ab8c3fe3e9c1c9113d02075a3
Chris Frank <chris@thenepotist.com>
2016-02-18 19:58:19 -0500

    added a query param to team

--- js/team.js
+++ js/team.js
@@ -1,16 +1,18 @@
 $(function(){
-	$(".team-link").click(function(e) {
-		e.preventDefault();
+  $(".team-link").click(function(e) {
+    e.preventDefault();
 
-   	if (!$(this).hasClass("active")) {
-    	$(".team-link.active").removeClass("active");
-    	$(this).addClass("active");
-  	}
+    if (!$(this).hasClass("active")) {
+      $(".team-link.active").removeClass("active");
+      $(this).addClass("active");
+    }
 
-  	var url = $(this).attr('href').replace('#', '').replace('team', 'teams');
+    var url = $(this).attr('href').replace('#', '').replace('team', 'teams');
+    // adding a query param to make sure github's cache busts
+    url += ("?" + new Date().getTime().toString())
 
-  	$.get(url, function(data){
- 			$('#main-team-content').html(data);
+    $.get(url, function(data){
+      $('#main-team-content').html(data);
     })
-	});
+  });
 });
